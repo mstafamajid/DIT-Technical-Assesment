629adaf88effb4e14c8a26fc3ffdb7376ffb4a7b
mustafa majid <95578220+mstafamajid@users.noreply.github.com>
2024-10-23 21:10:33 +0300

    clean up android

--- lib/feature/battery/domain/repositories/battery_repository.dart
+++ lib/feature/battery/domain/repositories/battery_repository.dart
@@ -1,6 +1,5 @@
 import 'package:dartz/dartz.dart';
 import 'package:dit_battery_status/feature/battery/data/models/batteryInfoModel.dart';
-import 'package:dit_battery_status/feature/battery/domain/entities/battery_status.dart';
 
 import '../../../../core/error/failure.dart';
 
