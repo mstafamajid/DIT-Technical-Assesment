0c1f0b49451c4b6c064886e3b3a2c5a90972de18
mustafa majid <95578220+mstafamajid@users.noreply.github.com>
2024-10-23 22:57:24 +0300

    create animation for circle progress indicator and color of it

--- lib/feature/battery/presentation/pages/main_page.dart
+++ lib/feature/battery/presentation/pages/main_page.dart
@@ -11,11 +11,47 @@ class MainPage extends StatefulWidget {
   State<MainPage> createState() => _MainPageState();
 }
 
-int? batteryLevel;
-bool? isCharging;
-BatteryBloc batteryBloc = di<BatteryBloc>();
+class _MainPageState extends State<MainPage>
+    with SingleTickerProviderStateMixin {
+  late final BatteryBloc batteryBloc;
+  late AnimationController _controller;
+  late Animation<double> _progressAnimation;
+  late Animation<Color?> _colorAnimation;
+  int? batteryLevel;
+  bool? isCharging;
+  double _previousBatteryLevel = 0;
 
-class _MainPageState extends State<MainPage> {
+  @override
+  void initState() {
+    super.initState();
+    batteryBloc = di<BatteryBloc>();
+
+    // Initialize Animation Controller
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+
+    // Initialize the progress animation; updated dynamically
+    _progressAnimation =
+        Tween<double>(begin: 0.0, end: 0.0).animate(_controller);
+  }
+
+  Color _getBatteryColor(double level) {
+    if (level > 0.5) {
+      return Color.lerp(Colors.yellow, Colors.green, (level - 0.5) * 2)!;
+    } else {
+      return Color.lerp(Colors.red, Colors.yellow, level * 2)!;
+    }
+  }
+
+  @override
+  void dispose() {
+    batteryBloc.close();
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,38 +63,95 @@ class _MainPageState extends State<MainPage> {
         child: BlocConsumer<BatteryBloc, BatteryState>(
           bloc: batteryBloc,
           listener: (context, state) {
-           print("BatteryBloc state: $state");
             if (state is BatteryInfoLoaded) {
-              batteryLevel = state.batteryInfo.batteryLevel;
-              isCharging = state.batteryInfo.isCharging;
+              setState(() {
+                batteryLevel = state.batteryInfo.batteryLevel;
+                isCharging = state.batteryInfo.isCharging;
 
-              setState(() {});
-            }else if(state is BatteryError){
-              print("Error: ${state.message}");
+                double newBatteryLevel = (batteryLevel ?? 0) / 100;
+
+                // Update progress animation between previous and new battery levels
+                _progressAnimation = Tween<double>(
+                  begin: _previousBatteryLevel,
+                  end: newBatteryLevel,
+                ).animate(_controller);
+
+                // Dynamically set the color tween based on battery levels
+                _colorAnimation = ColorTween(
+                  begin: _getBatteryColor(_previousBatteryLevel),
+                  end: _getBatteryColor(newBatteryLevel),
+                ).animate(_controller);
+
+                _previousBatteryLevel = newBatteryLevel;
+
+                // Animate progress and color
+                _controller.reset();
+                _controller.forward();
+              });
+            } else if (state is BatteryError) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text('Error: ${state.message}')),
+              );
             }
           },
-          builder: (context, state) => Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                  'Battery Level: ${batteryLevel ?? 'Unknown click on button to get battery info'}'),
-              Text(
-                  'Is Charging: ${isCharging ?? 'Unknown click on button to get battery info'}'),
-              const SizedBox(
-                height: 100,
-              ),
-              ElevatedButton(
+          builder: (context, state) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                if (batteryLevel != null)
+                  AnimatedBuilder(
+                    animation: _controller,
+                    builder: (context, child) {
+                      return Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          if (isCharging ?? false)
+                            Icon(
+                              Icons.bolt,
+                              size: 80,
+                              color: Colors.green,
+                            ),
+                          SizedBox(
+                            height: 100,
+                            width: 100,
+                            child: CircularProgressIndicator(
+                                backgroundColor: Colors.grey[300],
+                                strokeWidth: 5,
+                                value: _progressAnimation.value,
+                                valueColor: _colorAnimation),
+                          ),
+                        ],
+                      );
+                    },
+                  ),
+                const SizedBox(height: 20),
+                AnimatedBuilder(
+                  animation: _progressAnimation,
+                  builder: (context, a) => Text(
+                    'Battery Level: ${_progressAnimation.value == 0 ? 'Unknown' : (_progressAnimation.value * 100).toInt()}%',
+                    style: const TextStyle(fontSize: 18),
+                  ),
+                ),
+                Text(
+                  'Is Charging: ${isCharging == null ? 'Unknown' : (isCharging! ? 'Yes' : 'No')}',
+                  style: const TextStyle(fontSize: 18),
+                ),
+                const SizedBox(height: 50),
+                ElevatedButton(
                   onPressed: () {
                     batteryBloc.add(const GetBatteryInfoE());
                   },
-                  child: const Text('Get Battery Info')),
-              ElevatedButton(
+                  child: const Text('Get Battery Info'),
+                ),
+                ElevatedButton(
                   onPressed: () {
                     batteryBloc.add(const GetBatteryInfoE());
                   },
-                  child: const Text('Refresh button'))
-            ],
-          ),
+                  child: const Text('Refresh'),
+                ),
+              ],
+            );
+          },
         ),
       ),
     );
